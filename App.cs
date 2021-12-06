c04337c760e643cfcaf9cc19854a932f1d02e6f2
Hrishikesh <hrishikeshpramodnair@gmail.com>
2021-12-06 21:17:13 +0530

    Added Main Menu, Cube addition, meshes drag & drop functionality

--- App.cs
+++ App.cs
@@ -1,5 +1,8 @@
 ﻿using StereoKit;
 using System.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StereoKitApp
 {
@@ -16,7 +19,21 @@ namespace StereoKitApp
 		Matrix4x4 floorTransform = Matrix.TS(new Vector3(0, -1.5f, 0), new Vector3(30, 0.1f, 30));
 		Material  floorMaterial;
 
+		// Menu parameters
 		Pose ObjectWindowPose = new Pose(0f, 0f, 0f, Quat.Identity);
+		// Main Menu
+		bool isMainMenu = false;
+		bool isPlaceOb = false;
+		bool isImportSc = false;
+		bool isExportSc = false;
+		// Place Menu
+		bool isPlaceMenu = false;
+
+		// Mesh Lists
+		List<Mesh> meshList = new List<Mesh>();
+		List<Pose> meshPoses = new List<Pose>();
+		List<string> meshHandles = new List<string>();
+		int meshCount = 0;
 
 		public void Init()
 		{
@@ -27,7 +44,11 @@ namespace StereoKitApp
 
 			floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
 			floorMaterial.Transparency = Transparency.Blend;
-		}
+
+			// setting default values
+			isMainMenu = true;
+			meshCount = 0;
+		}//Init
 
 		public void Step()
 		{
@@ -37,9 +58,89 @@ namespace StereoKitApp
 			UI.Handle("Cube", ref cubePose, cube.Bounds);
 			cube.Draw(cubePose.ToMatrix());
 
-			// Object inserter UI
-			UI.WindowBegin("Insert", ref ObjectWindowPose, new Vector2(24, 0) * U.cm);
-			UI.WindowEnd();
-		}
-	}
+			UIDisplay();
+			RenderMeshes();
+			MeshUIs();
+            
+		}//Step
+
+		// fn to reset all UI vars to false
+		void resetMainUIParams()
+        {
+			isMainMenu = false;
+			isPlaceOb = false;
+			isImportSc = false;
+			isExportSc = false;
+        }//resetMainUIParams
+
+		void UIDisplay()
+        {
+            #region MainMenu
+            // Object inserter UI
+            if (isMainMenu)
+			{
+				UI.WindowBegin("Main Menu", ref ObjectWindowPose, new Vector2(24, 0) * U.cm);
+				{
+					if (UI.Button("Place Object"))
+					{
+						resetMainUIParams();
+						isPlaceOb = true;
+					};
+					if (UI.Button("Import Scene"))
+					{
+						resetMainUIParams();
+						isImportSc = true;
+					};
+					if (UI.Button("Export Scene"))
+					{
+						resetMainUIParams();
+						isExportSc = true;
+					};
+					if (UI.Button("Exit"))
+					{
+						SK.Quit();
+					}
+				}
+				UI.WindowEnd();
+			}
+            #endregion
+            if (isPlaceOb)
+            {
+				UI.WindowBegin("Place", ref ObjectWindowPose, new Vector2(24, 0) * U.cm);
+                if (UI.Button("Cube"))
+                {
+					Mesh m = Mesh.GenerateCube(Vec3.One*0.04f);
+					meshList.Add(m);
+					Pose mPose = new Pose(0, 0, -0.5f, Quat.Identity);
+					meshPoses.Add(mPose);
+					string mHandle = "Mesh" + meshCount.ToString();
+					meshHandles.Add(mHandle);
+					meshCount++;
+				}
+				UI.WindowEnd();
+			}
+        }//UIDisplay
+
+		void RenderMeshes()
+        {
+			for(int i = 0; i < meshList.Count; i++)
+            {
+				Matrix meshTransform = meshPoses[i].ToMatrix();
+				Mesh m = meshList[i];
+				m.Draw(Default.Material, meshTransform);
+            }
+        }//RenderMeshes
+
+		void MeshUIs()
+        {
+			for(int i = 0; i < meshList.Count; i++)
+            {
+				Pose meshPose = meshPoses[i];
+				if(UI.Handle(meshHandles[i], ref meshPose, meshList[i].Bounds))
+                {
+					meshPoses[i] = meshPose;
+				}
+            }
+        }//MeshUIs
+    }
 }
