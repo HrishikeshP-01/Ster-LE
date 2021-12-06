614be0f601431ef33bab2092a52aa9e437afa594
Hrishikesh <hrishikeshpramodnair@gmail.com>
2021-12-07 04:35:13 +0530

    Added vertex count & dimension to export functionality

--- SceneTransfer.cs
+++ SceneTransfer.cs
@@ -16,15 +16,29 @@ namespace StereoKitApp
 			{
 				Mesh m = meshList[i];
 				Pose mp = meshPoses[i];
+
+				// Add mesh type & position to file
 				float lx = mp.position.x;
 				float ly = mp.position.y;
 				float lz = mp.position.z;
 				data += meshType[i].ToString() + "," + lx.ToString() + "," + ly.ToString() + "," + lz.ToString() + ",";
+
+				// Add orientation to file
 				float qx = mp.orientation.x;
 				float qy = mp.orientation.y;
 				float qz = mp.orientation.z;
 				float qw = mp.orientation.w;
 				data += qx.ToString() + "," + qy.ToString() + "," + qz.ToString() + "," + qw.ToString() + ",";
+
+				// Add dimension info to file
+				float sx = m.Bounds.dimensions.x;
+				float sy = m.Bounds.dimensions.y;
+				float sz = m.Bounds.dimensions.z;
+				data += sx.ToString() + "," + sy.ToString() + "," + sz.ToString() + "," ;
+
+				// Add vertex count to file
+				float vc = m.GetVerts().Length;
+				data += vc.ToString() + ",";
 				data += "\n";
 			}
 			return data;
